2c1d0b072d870694c9153ede43c34b2008e5716b
Ibrahim Alassaf <xibra9141@gmail.com>
2022-08-25 11:09:07 +0300

    fix: duplicate identifier ''dark''.

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -4,7 +4,7 @@ import { ImSpinner2 } from 'react-icons/im';
 import clsxm from '@/lib/clsxm';
 
 enum ButtonVariant {
-  'dark',
+  'primary',
   'outline',
   'ghost',
   'light',
@@ -37,7 +37,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         disabled={disabled}
         className={clsxm(
           'inline-flex items-center rounded px-4 py-2 font-semibold',
-          'focus:outline-none focus-visible:ring focus-visible:ring-dark-500',
+          'focus:outline-none focus-visible:ring focus-visible:ring-primary-500',
           'shadow-sm',
           'transition-colors duration-75',
           //#region  //*=========== Variants ===========
